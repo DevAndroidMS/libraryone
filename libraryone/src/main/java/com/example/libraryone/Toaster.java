89adc4c5716debb4e485fda24bccb8d463eccc05
anupk121 <anupkhedekar34@gmail.com>
2019-09-27 17:57:13 +0530

    Initial commit

---
+++ libraryone/src/main/java/com/example/libraryone/Toaster.java
@@ -0,0 +1,11 @@
+package com.example.libraryone;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public void showMessage(Context ctx, String string) {
+        Toast.makeText(ctx, string, Toast.LENGTH_LONG).show();
+    }
+}
